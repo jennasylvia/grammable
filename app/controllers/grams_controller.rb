52751f20a16354d3c87cf9a9a059abc6d05761ab
Jenna Sylvia <jenna.sylvia@berkeley.edu>
2020-01-10 05:07:08 +0000

    basically everything, images and styling

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,37 +1,28 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
-  def update
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-    @gram.update_attributes(gram_params)
-    
-    if @gram.valid?
-      redirect_to root_path
-    else
-      return render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def new
-    @gram = Gram.new
-  end
-  
   def index
+    @grams = Gram.all
   end
 
   def show
-     @gram = Gram.find_by_id(params[:id])
-     return render_not_found if @gram.blank?
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      return render_not_found if @gram.blank?
+    end
   end
 
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
-  def create 
+  def new
+    @gram = Gram.new
+  end
+
+  def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
       redirect_to root_path
@@ -40,14 +31,35 @@ class GramsController < ApplicationController
     end
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+
+    @gram.update_attributes(gram_params)
+    
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end 
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
+  end
+ 
+  
+
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
-  end
-
+  
 end
