7d44142186e50203d1514002ba0c9f2f573052cd
Jenna Sylvia <jenna.sylvia@berkeley.edu>
2020-02-01 05:08:53 +0000

    ufhaiuwehf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_045512) do
+ActiveRecord::Schema.define(version: 2020_02_01_050025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_01_045512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.json "picture"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
